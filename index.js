8b333c9118b15a9b0776b809408b4a2d6f5b4cf9
Lismei5429 <fernandezjorge0156@gmail.com>
2025-07-08 07:30:44 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Token del bot
+const token = '8120539310:AAE0S0xrESMcIbe9QFyLc6ZVStlKflsiYIk';
+
+// Crear el bot
+const bot = new TelegramBot(token, { polling: true });
+
+// Comando /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const bienvenida = 👋 ¡Hola ${msg.from.first_name}!\n\nBienvenido a la plataforma MinSpaceBot 🚀\n\nPulsa el botón de abajo para abrir la mini aplicación.;
+
+  const opciones = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🌐 Abrir MiniApp",
+            web_app: { url: "https://minspacebot.onrender.com" }
+          }
+        ]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, bienvenida, opciones);
+});
